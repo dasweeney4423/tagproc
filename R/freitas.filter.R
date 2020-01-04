b2817d76ca3b0a7852fb5002a956eb67c770409e
dasweeney4423 <dasweeney4423@gmail.com>
2020-01-04 13:43:11 -0800

    created help information

--- R/freitas.filter.R
+++ R/freitas.filter.R
@@ -1,6 +1,16 @@
-freitas.filter <- function(argos, vmax, ang = c(15, 25), distlim = c(3000,3000)) {
-  argos <- argos[!is.na(argos$Latitude) & !is.na(argos$Longitude),]
+#' Perform the freitas filtering method
+#'
+#' This function filters location data obtained from Argos using the Freitas et al. (2008) algorithm.
+#' @param argos A dataframe of argos locations to be sent through the Freitas filer algorithm
+#' @param vmax Speed threshold for filter method, in m/s. Default is 2 m/s
+#' @param ang Angles of the spikes to be removed by freitas filter. Default is c(15, 25). No spikes are removed if ang=-1
+#' @param distlim Lengths of the above spikes (see input 'ang'), in kilometers. Default is c(2.5, 5).
+#' @return A dataframe of filtered locations
+#' @examples #examples not yet provided, sorry :(
 
+freitas.filter <- function(argos, vmax = 2, ang = c(15, 25), distlim = c(2.5, 5)) {
+  argos <- argos[!is.na(argos$Latitude) & !is.na(argos$Longitude),]
+  distlim <- distlim * 1000
   if (nrow(argos) > 0) {
     D <- as.character(argos$Date)
     filter <- argosfilter::sdafilter(lat = argos$Latitude,
--- R/gps.filter.R
+++ R/gps.filter.R
@@ -1,3 +1,12 @@
+#' Perform a simple GPS filter
+#'
+#' This function filters GPS location data using a specified threshold for the number of satellites and residual values
+#' @param gps A dataframe of gps locations to be sent through the filter
+#' @param min.sat Minimum number of satellites for that location for it to be kept. Default is 5
+#' @param max.residual Maximum residual value. For a location to be kept, its residual must be less than or equal to this input. Default is 35 (Dujon et al. 2014)
+#' @return A dataframe of filtered locations
+#' @examples #examples not yet provided, sorry :(
+
 gps.filter <- function(gps, min.sat = 5, max.residual = 35) {
   gps <- gps[!is.na(gps$Latitude) & !is.na(gps$Longitude),]
 
