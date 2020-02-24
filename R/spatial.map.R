c21478e90f55e9837556bd6db977d123fd78b34f
dasweeney4423 <dasweeney4423@gmail.com>
2020-02-24 11:19:28 -0800

    reformatted mapping

--- R/spatial.map.R
+++ R/spatial.map.R
@@ -1,9 +1,4 @@
-spatial.map <- function(data, loc.color = 'black', loc.size = 1, title = NULL, subtitle = NULL,
-                        spatial.size = NULL, spatial.color = NULL, spatial.fill = NULL, ...) {
-  #Make sure the variable is projected as CRS = WGS84
-  data.sp <- sp::SpatialPoints(data[c("Longitude", "Latitude")])
-  proj4string(data.sp) = sp::CRS("+init=epsg:4326")
-  data.sp <- sp::spTransform(data.sp, CRS("+init=epsg:4326"))
+spatial.map <- function(..., color = NULL, size = NULL, title = NULL, subtitle = NULL, crs = NULL) {
 
   #get earth tiles
   esri_ocean <- paste0('https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/${z}/${y}/${x}.jpeg')
@@ -12,17 +7,37 @@ spatial.map <- function(data, loc.color = 'black', loc.size = 1, title = NULL, s
   suppressWarnings(suppressPackageStartupMessages(require(ggplot2)))
   p <- ggplot() +
           annotation_map_tile(type = esri_ocean, zoomin = 1, progress = "none") +
-          layer_spatial(data = zcdata_cat.sp, size = loc.size, color = loc.color)
+
+  for (l in 1:...length()) {
+    data.layer <- list(...)[[l]]
+    if (!is.null(crs)) {
+      data.layer <- sp::spTransform(data.layer, sp::CRS(crs))
+    } else {
+      if (l == 1) {
+        crs1 <- proj4string(data.layer)
+      } else {
+        if (proj4string(data.layer) != crs1) {
+          data.layer <- sp::spTransform(data.layer, sp::CRS(crs1))
+        }
+      }
+    }
+
+    if (class(data.layer) == "SpatialPoints") {
+      p <- p + layer_spatial(data = data.layer, color = color[l], size = size[l])
+    } else {
+      if (class(data.layer) == "SpatialPolygonsDataFrame") {
+
+        p <- p + layer_spatial(data = data.layer, size = size[l], color = color[l], spatial.fill = 'transparent')
+      }
+    }
+  }
+
+  #include titles if desired
   if (!is.null(title)) {
     p <- p + ggtitle(title)
   }
   if (!is.null(subtitle)) {
     p <- p + ggtitle(subtitle = subtitle)
-  }
-  if (...length() > 0) {
-    for (l in 1:...length()) {
-      p <- p + layer_spatial(data = list(...)[[l]], size = spatial.size[l], color = spatial.color[l], fill = spatial.fill[l])
-    }
   }
 
   #plot it all
