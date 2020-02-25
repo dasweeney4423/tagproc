fed5bdebf480bb67bc10a9301809805c07069543
dasweeney4423 <dasweeney4423@gmail.com>
2020-02-25 13:26:48 -0800

    typos fixes and bug with titles

--- R/spatial.map.R
+++ R/spatial.map.R
@@ -4,9 +4,8 @@ spatial.map <- function(..., color = NULL, size = NULL, title = NULL, subtitle =
   esri_ocean <- paste0('https://services.arcgisonline.com/arcgis/rest/services/Ocean/World_Ocean_Base/MapServer/tile/${z}/${y}/${x}.jpeg')
 
   #create all layers
-  suppressWarnings(suppressPackageStartupMessages(require(ggplot2)))
-  p <- ggplot() +
-          annotation_map_tile(type = esri_ocean, zoomin = 1, progress = "none") +
+  p <- ggplot2::ggplot() +
+          ggspatial::annotation_map_tile(type = esri_ocean, zoomin = 1, progress = "none")
 
   for (l in 1:...length()) {
     data.layer <- list(...)[[l]]
@@ -14,30 +13,31 @@ spatial.map <- function(..., color = NULL, size = NULL, title = NULL, subtitle =
       data.layer <- sp::spTransform(data.layer, sp::CRS(crs))
     } else {
       if (l == 1) {
-        crs1 <- proj4string(data.layer)
+        crs1 <- sp::proj4string(data.layer)
       } else {
-        if (proj4string(data.layer) != crs1) {
+        if (sp::proj4string(data.layer) != crs1) {
           data.layer <- sp::spTransform(data.layer, sp::CRS(crs1))
         }
       }
     }
 
     if (class(data.layer) == "SpatialPoints") {
-      p <- p + layer_spatial(data = data.layer, color = color[l], size = size[l])
+      p <- p + ggspatial::layer_spatial(data = data.layer, color = color[l], size = size[l])
     } else {
       if (class(data.layer) == "SpatialPolygonsDataFrame") {
 
-        p <- p + layer_spatial(data = data.layer, size = size[l], color = color[l], spatial.fill = 'transparent')
+        p <- p + ggspatial::layer_spatial(data = data.layer, size = size[l], color = color[l], spatial.fill = 'transparent')
       }
     }
   }
 
   #include titles if desired
   if (!is.null(title)) {
-    p <- p + ggtitle(title)
-  }
-  if (!is.null(subtitle)) {
-    p <- p + ggtitle(subtitle = subtitle)
+    if (!is.null(subtitle)) {
+      p <- p + ggplot2::ggtitle(title, subtitle = subtitle)
+    } else {
+      p <- p + ggplot2::ggtitle(title)
+    }
   }
 
   #plot it all
