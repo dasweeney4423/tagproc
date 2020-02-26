a274886e16921d72e7cff08b6f828ed62d9dc322
dasweeney4423 <dasweeney4423@gmail.com>
2020-02-26 11:51:58 -0800

    allowed x-axis to be changed

--- R/plot.seafloor.R
+++ R/plot.seafloor.R
@@ -2,10 +2,11 @@
 #'
 #' Plot bathymetry data that has been collected from the bathy.sync()) function
 #' @param data A dataframe obtained after using the bathy.sync() function that contains bathymetry data associated with tag data
+#' @param xlab Optional input to specify desired x-axis label for the plot. Default is "Location Record"
 #' @return A multipanelled figure plotting important bathymetry variables
 #' @examples #examples not yet provided, sorry :(
 
-plot.seafloor <- function(data) {
+plot.seafloor <- function(data, xlab = "Location Record") {
   par(mfrow=c(2, 1))
 
   # Seafloor depth
@@ -21,5 +22,5 @@ plot.seafloor <- function(data) {
   axis(1, cex.axis = 1.5)
   axis(2, las = 2, cex.axis = 1.5)
   title(ylab = "% Slope", cex.lab = 1.5, line = 4)
-  title(xlab = "Dive record", cex.lab = 1.5)
+  title(xlab = xlab, cex.lab = 1.5)
 }
