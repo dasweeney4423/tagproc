30598f6d5b19ddf9bb20ecb3b0d2dd5a78bc40fa
dasweeney4423 <dasweeney4423@gmail.com>
2020-02-26 11:47:25 -0800

    fixed x axis and allowed for it to be an optional input

--- R/plot.sunmoon.R
+++ R/plot.sunmoon.R
@@ -7,7 +7,7 @@
 
 plot.sunmoon <- function(data) {
   par(mfrow = c(4, 1))
-  par(mar = c(1, 6, .5, .5), oma = c(2, 2, 2, 2))
+  par(mar=c(.5,6,.5,.5), oma=c(4,2,2,2))
   cex.scale = 1
 
   plot(data$sunAzimuth, type = "o", pch = 16, cex = .5, ylim = c(0, 360), col = "orange", ann = FALSE, axes = FALSE)
@@ -25,5 +25,6 @@ plot.sunmoon <- function(data) {
   plot(data$moonPhase, type = "o", pch = 16, cex = .5, col = "firebrick", ann = FALSE, axes = FALSE)
   axis(2, las = 2, cex.axis = cex.scale)
   axis(1, cex.axis = cex.scale)
-  title(ylab = "Moon\nPhase", xlab = "Dive record", cex.lab = cex.scale)
+  title(ylab = "Moon\nPhase", cex.lab = cex.scale)
+  title(xlab = "Dive record", cex.lab = cex.scale, outer=T, line = 2)
 }
