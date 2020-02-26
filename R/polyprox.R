4159f1ee76bc1b2cca988830c0ed3261967e3365
dasweeney4423 <dasweeney4423@gmail.com>
2020-02-26 14:12:23 -0800

    working on converting this to allow for single input or folder filled with polygons

--- R/polyprox.R
+++ R/polyprox.R
@@ -1,25 +1,17 @@
-polyprox <- function(env, polypath) {
-  polylf <- list.files(polypath)
-  polylf <- polylf[polylf != "zips"]
-
-  # Format polygon names
-  polynames <- gsub(".kml", "", polylf)
-  polynames <- gsub(" ", "", polynames)
-  polynames <- gsub("-", "", polynames)
-
+polyprox <- function(data, poly) {
   # Format pos/bhvr data
-  LAT <- env[, which(names(env) %in% c("Latitude", "StartLat"))]
-  LON <- env[, which(names(env) %in% c("Longitude", "StartLon"))]
+  LAT <- data[, which(names(data) %in% c("Latitude", "StartLat"))]
+  LON <- data[, which(names(data) %in% c("Longitude", "StartLon"))]
 
-  # Loop through each Polygon
-  for (j in 1:length(polylf)) {
-    # Load polygon j
-    polyname <- paste0(polypath, "/", polylf[j])
+  polylf <- list.files(poly)
+  if (length(polylf) == 0) {
+    polyname <- poly
+    # Load polygon
     polj <- rgdal::readOGR(dsn = polyname)
 
     # Loop through each GPS record
-    polydist <- rep(NA, times = nrow(env))
-    for (i in 1:nrow(env)) {
+    polydist <- rep(NA, times = nrow(data))
+    for (i in 1:nrow(data)) {
       # Collect lat and long
       x <- LON[i]
       y <- LAT[i]
@@ -43,9 +35,45 @@ polyprox <- function(env, polypath) {
     } # end of GPS loop
 
     # Add distance vector to GPS dataframe
-    env$poly <- polydist
-    names(env)[which(names(env) == "poly")] <- polynames[j]
-  } # end of polygon loop
+    data$poly <- polydist
+    names(data)[which(names(data) == "poly")] <- polynames
+  } else {
+    # Loop through each Polygon
+    for (j in 1:length(polylf)) {
+      # Load polygon j
+      polyname <- paste0(poly, "/", polylf[j])
+      polj <- rgdal::readOGR(dsn = polyname)
 
-  return(env)
+      # Loop through each GPS record
+      polydist <- rep(NA, times = nrow(data))
+      for (i in 1:nrow(data)) {
+        # Collect lat and long
+        x <- LON[i]
+        y <- LAT[i]
+
+        if (!any(is.na(c(x, y)))) {
+          # Format spatial datasets
+          coord <- data.frame(longitude = x, latitude = y)
+          sp::coordinates(coord) <- c("longitude", "latitude")
+          sp::proj4string(coord) <- sp::proj4string(polj)
+
+          # Test whether point is in polygon (if FALSE, it is not)
+          inpoly <- !is.na(over(coord, as(polj, "SpatialPolygons")))
+          if (inpoly) {
+            polydist[i] <- 0 # If pt is in polygon, set distance to 0
+          } else {
+            dij <- geosphere::dist2Line(p = coord, line = polj)  # If pt is not in polygon, determine distance to polygon
+            distij <- round(dij[1] / 1000, digits = 3)  # Format distance
+            polydist[i] <- distij
+          }
+        } # end of if x | y is na
+      } # end of GPS loop
+
+      # Add distance vector to GPS dataframe
+      data$poly <- polydist
+      names(data)[which(names(data) == "poly")] <- polynames[j]
+    } # end of polygon loop
+  }
+
+  return(data)
 }
