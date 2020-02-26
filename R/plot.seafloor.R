16a57643c7a6da34356671fb915da925d5f9610a
dasweeney4423 <dasweeney4423@gmail.com>
2020-02-26 12:13:05 -0800

    made axes not so dang big

--- R/plot.seafloor.R
+++ R/plot.seafloor.R
@@ -12,15 +12,15 @@ plot.seafloor <- function(data, xlab = "Location Record") {
   # Seafloor depth
   par(mar = c(3, 6, .5, .5))
   plot(data$z, lwd = 3, ylim = c(max(data$z, na.rm = TRUE), 0), col = "steelblue1", type = "l", axes = FALSE, ann = FALSE)
-  axis(1, cex.axis = 1.5)
-  axis(2, las = 2, cex.axis = 1.5)
-  title(ylab = "Depth (m)", cex.lab = 1.5, line = 4)
+  axis(1, cex.axis = 1)
+  axis(2, las = 2, cex.axis = 1)
+  title(ylab = "Depth (m)", cex.lab = 1, line = 4)
 
   # Seafloor slope
   par(mar = c(5, 6, .5, .5))
   plot(data$zslope, lwd = 3, ylim = c(0, max(data$zslope, na.rm = TRUE)), col = "purple", type = "l", axes = FALSE, ann = FALSE)
-  axis(1, cex.axis = 1.5)
-  axis(2, las = 2, cex.axis = 1.5)
-  title(ylab = "% Slope", cex.lab = 1.5, line = 4)
-  title(xlab = xlab, cex.lab = 1.5)
+  axis(1, cex.axis = 1)
+  axis(2, las = 2, cex.axis = 1)
+  title(ylab = "% Slope", cex.lab = 1, line = 4)
+  title(xlab = xlab, cex.lab = 1)
 }
